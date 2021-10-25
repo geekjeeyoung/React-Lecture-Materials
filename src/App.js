c21b8ba2742f9504330daa125d97b400d1e77bb4
Jeeyoung Han <64334381+hancandice@users.noreply.github.com>
2021-10-25 17:36:46 +0900

    Lesson 1. 리액트 역사 ~ 조건부 렌더링

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Hello from "./Hello";
+import Wrapper from "./Wrapper";
 
 function App() {
+  const name = "Jeeyoung";
+  const style = {
+    backgroundColor: "black",
+    color: "aqua",
+    fontSize: 24,
+    padding: "1rem",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Hello name="react" color="red" isSpecial />
+      <Hello color="pink" />
+      <div style={style}>{name}</div>
+      <div className="gray-box"></div>
+    </Wrapper>
   );
 }
 
