223ac01e366601127eb130e83e4fcd59f875595c
Jeeyoung Han <64334381+hancandice@users.noreply.github.com>
2021-10-27 17:39:01 +0900

    Lesson 3. useRef 로 컴포넌트 안의 변수 만들기 ~ 배열 항목 제거 및 수정하기

--- src/App.js
+++ src/App.js
@@ -1,20 +1,90 @@
+import { useRef, useState } from "react";
 import "./App.css";
-import InputSample from "./InputSample";
-import Wrapper from "./Wrapper";
+import CreateUser from "./CreateUser";
+import UserList from "./UserList";
 
 function App() {
-  const name = "Jeeyoung";
-  const style = {
-    backgroundColor: "black",
-    color: "aqua",
-    fontSize: 24,
-    padding: "1rem",
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+  });
+
+  const { username, email } = inputs;
+
+  const originalUsers = [
+    {
+      id: 1,
+      username: "Jeeyoung",
+      email: "jeeyoung.han82@gmail.com",
+      active: true,
+    },
+    {
+      id: 2,
+      username: "Candice",
+      email: "candice@example.com",
+      active: false,
+    },
+    {
+      id: 3,
+      username: "han",
+      email: "han@example.com",
+      active: false,
+    },
+  ];
+
+  const [users, setUsers] = useState(originalUsers);
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+    const newUser = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    setUsers(users.concat(newUser));
+    setInputs({
+      username: "",
+      email: "",
+    });
+    nextId.current += 1;
+  };
+
+  const onRemove = (id) => {
+    setUsers(users.filter((user) => user.id !== id));
+  };
+
+  const onToggle = (id) => {
+    setUsers(
+      users.map((user) =>
+        user.id === id
+          ? {
+              ...user,
+              active: !user.active,
+            }
+          : user
+      )
+    );
   };
 
   return (
-    <Wrapper>
-      <InputSample />
-    </Wrapper>
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+    </>
   );
 }
 
